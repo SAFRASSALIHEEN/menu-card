ab00165584adaf1acace080ffb93440575cf7ef4
developer <developer@zecode.lk>
2025-06-28 00:24:41 +0530

    Fix issues

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 // import Foodmenu from './components/food-menu'
-import CoffeeCard from './components/coffee-card'
+// import CoffeeCard from './components/coffee-card'
+import DrinksMenu from './components/coffee-card'
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
     <>
       <div className="min-h-screen bg-gray-100">
         <h1 className="text-3xl font-bold text-center py-8 text-[#101828]">Our Signature Dishes</h1>
-        <CoffeeCard />
+        <DrinksMenu />
       </div>
     </>
   )
