f6d7bccb157b649be4568fddc9400b1633b6938f
developer <developer@zecode.lk>
2025-06-28 00:35:51 +0530

    Fix image paths and update deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import Foodmenu from './components/food-menu'
+// import Food/menu from './components/food-menu'
 // import CoffeeCard from './components/coffee-card'
 import DrinksMenu from './components/coffee-card'
 
--- src/components/coffee-card.jsx
+++ src/components/coffee-card.jsx
@@ -87,12 +87,12 @@ function DrinksMenu() {
             textColor: "text-[#8B4513]",
             borderColor: "border-[#8B4513]",
             buttonColor: "bg-[#D2691E]",
-            mainImage: "/assets/coffee.png",
+            mainImage: "/menu-card/imgs/coffee.png",
             ingredients: [
-                { image: "/assets/bean.png", alt: "bean", position: "bottom-28 left-20", size: "w-8 h-8", x: 80, y: -35, rotate: 45, duration: 1.2, delay: 0.2 },
-                { image: "/assets/bean.png", alt: "bean", position: "bottom-32 left-24", size: "w-7 h-7", x: 90, y: -45, rotate: -30, duration: 1.4, delay: 0.4 },
-                { image: "/assets/bean.png", alt: "bean", position: "bottom-24 left-28", size: "w-7 h-7", x: 100, y: -30, rotate: 90, duration: 1.6, delay: 0.6 },
-                { image: "/assets/choco.png", alt: "chocolate", position: "bottom-30 left-22", size: "w-7 h-7", x: 85, y: -40, rotate: 180, duration: 1.3, delay: 0.5 }
+                { image: "/menu-card/imgs/bean.png", alt: "bean", position: "bottom-28 left-20", size: "w-8 h-8", x: 80, y: -35, rotate: 45, duration: 1.2, delay: 0.2 },
+                { image: "/menu-card/imgs/bean.png", alt: "bean", position: "bottom-32 left-24", size: "w-7 h-7", x: 90, y: -45, rotate: -30, duration: 1.4, delay: 0.4 },
+                { image: "/menu-card/imgs/bean.png", alt: "bean", position: "bottom-24 left-28", size: "w-7 h-7", x: 100, y: -30, rotate: 90, duration: 1.6, delay: 0.6 },
+                { image: "/menu-card/imgs/choco.png", alt: "chocolate", position: "bottom-30 left-22", size: "w-7 h-7", x: 85, y: -40, rotate: 180, duration: 1.3, delay: 0.5 }
             ]
         },
         {
@@ -102,12 +102,12 @@ function DrinksMenu() {
             textColor: "text-[#2e7d32]",
             borderColor: "border-[#2e7d32]",
             buttonColor: "bg-[#4caf50]",
-            mainImage: "/assets/green-tea.png",
+            mainImage: "/menu-card/imgs/green-tea.png",
             ingredients: [
-                { image: "/assets/tea-leaf.png", alt: "tea leaf", position: "bottom-28 left-20", size: "w-8 h-8", x: 75, y: -40, rotate: 30, duration: 1.1, delay: 0.2 },
-                { image: "/assets/tea-leaf.png", alt: "tea leaf", position: "bottom-32 left-24", size: "w-7 h-7", x: 85, y: -50, rotate: -45, duration: 1.3, delay: 0.4 },
-                { image: "/assets/mint.png", alt: "mint", position: "bottom-24 left-28", size: "w-7 h-7", x: 95, y: -35, rotate: 60, duration: 1.5, delay: 0.6 },
-                { image: "/assets/mint.png", alt: "mint", position: "bottom-36 left-16", size: "w-6 h-6", x: 70, y: -55, rotate: 120, duration: 1.0, delay: 0.3 }
+                { image: "/menu-card/imgs/tea-leaf.png", alt: "tea leaf", position: "bottom-28 left-20", size: "w-8 h-8", x: 75, y: -40, rotate: 30, duration: 1.1, delay: 0.2 },
+                { image: "/menu-card/imgs/tea-leaf.png", alt: "tea leaf", position: "bottom-32 left-24", size: "w-7 h-7", x: 85, y: -50, rotate: -45, duration: 1.3, delay: 0.4 },
+                { image: "/menu-card/imgs/mint.png", alt: "mint", position: "bottom-24 left-28", size: "w-7 h-7", x: 95, y: -35, rotate: 60, duration: 1.5, delay: 0.6 },
+                { image: "/menu-card/imgs/mint.png", alt: "mint", position: "bottom-36 left-16", size: "w-6 h-6", x: 70, y: -55, rotate: 120, duration: 1.0, delay: 0.3 }
             ]
         },
         {
@@ -117,12 +117,12 @@ function DrinksMenu() {
             textColor: "text-[#c2185b]",
             borderColor: "border-[#c2185b]",
             buttonColor: "bg-[#e91e63]",
-            mainImage: "/assets/smoothie.png",
+            mainImage: "/menu-card/imgs/smoothie.png",
             ingredients: [
-                { image: "/assets/strawberry.png", alt: "strawberry", position: "bottom-28 left-20", size: "w-8 h-8", x: 80, y: -35, rotate: 45, duration: 1.2, delay: 0.2 },
-                { image: "/assets/mango.png", alt: "mango", position: "bottom-32 left-24", size: "w-7 h-7", x: 90, y: -45, rotate: -30, duration: 1.4, delay: 0.4 },
-                { image: "/assets/strawberry.png", alt: "banana", position: "bottom-24 left-28", size: "w-7 h-7", x: 100, y: -30, rotate: 90, duration: 1.6, delay: 0.6 },
-                { image: "/assets/mango.png", alt: "blueberry", position: "bottom-36 left-16", size: "w-6 h-6", x: 70, y: -50, rotate: 120, duration: 1.0, delay: 0.3 }
+                { image: "/menu-card/imgs/strawberry.png", alt: "strawberry", position: "bottom-28 left-20", size: "w-8 h-8", x: 80, y: -35, rotate: 45, duration: 1.2, delay: 0.2 },
+                { image: "/menu-card/imgs/mango.png", alt: "mango", position: "bottom-32 left-24", size: "w-7 h-7", x: 90, y: -45, rotate: -30, duration: 1.4, delay: 0.4 },
+                { image: "/menu-card/imgs/strawberry.png", alt: "banana", position: "bottom-24 left-28", size: "w-7 h-7", x: 100, y: -30, rotate: 90, duration: 1.6, delay: 0.6 },
+                { image: "/menu-card/imgs/mango.png", alt: "blueberry", position: "bottom-36 left-16", size: "w-6 h-6", x: 70, y: -50, rotate: 120, duration: 1.0, delay: 0.3 }
             ]
         },
         {
@@ -132,12 +132,12 @@ function DrinksMenu() {
             textColor: "text-[#f57c00]",
             borderColor: "border-[#f57c00]",
             buttonColor: "bg-[#ff9800]",
-            mainImage: "/assets/orange-juice.png",
+            mainImage: "/menu-card/imgs/orange-juice.png",
             ingredients: [
-                { image: "/assets/orange.png", alt: "orange", position: "bottom-28 left-20", size: "w-8 h-8", x: 85, y: -40, rotate: 30, duration: 1.2, delay: 0.2 },
-                { image: "/assets/lemon.png", alt: "orange slice", position: "bottom-32 left-24", size: "w-7 h-7", x: 75, y: -50, rotate: -60, duration: 1.4, delay: 0.4 },
-                { image: "/assets/orange.png", alt: "orange", position: "bottom-24 left-28", size: "w-7 h-7", x: 95, y: -35, rotate: 90, duration: 1.6, delay: 0.6 },
-                { image: "/assets/lemon.png", alt: "lemon", position: "bottom-36 left-16", size: "w-6 h-6", x: 70, y: -55, rotate: 180, duration: 1.0, delay: 0.3 }
+                { image: "/menu-card/imgs/orange.png", alt: "orange", position: "bottom-28 left-20", size: "w-8 h-8", x: 85, y: -40, rotate: 30, duration: 1.2, delay: 0.2 },
+                { image: "/menu-card/imgs/lemon.png", alt: "orange slice", position: "bottom-32 left-24", size: "w-7 h-7", x: 75, y: -50, rotate: -60, duration: 1.4, delay: 0.4 },
+                { image: "/menu-card/imgs/orange.png", alt: "orange", position: "bottom-24 left-28", size: "w-7 h-7", x: 95, y: -35, rotate: 90, duration: 1.6, delay: 0.6 },
+                { image: "/menu-card/imgs/lemon.png", alt: "lemon", position: "bottom-36 left-16", size: "w-6 h-6", x: 70, y: -55, rotate: 180, duration: 1.0, delay: 0.3 }
             ]
         },
         {
@@ -147,12 +147,12 @@ function DrinksMenu() {
             textColor: "text-[#5d4037]",
             borderColor: "border-[#5d4037]",
             buttonColor: "bg-[#795548]",
-            mainImage: "/assets/hot-chocolate.png",
+            mainImage: "/menu-card/imgs/hot-chocolate.png",
             ingredients: [
-                { image: "/assets/choco.png", alt: "chocolate", position: "bottom-28 left-20", size: "w-8 h-8", x: 80, y: -35, rotate: 45, duration: 1.2, delay: 0.2 },
-                { image: "/assets/milk.png", alt: "milk", position: "bottom-32 left-24", size: "w-7 h-7", x: 90, y: -45, rotate: -30, duration: 1.4, delay: 0.4 },
-                { image: "/assets/choco.png", alt: "marshmallow", position: "bottom-24 left-28", size: "w-7 h-7", x: 100, y: -30, rotate: 90, duration: 1.6, delay: 0.6 },
-                { image: "/assets/milk.png", alt: "cinnamon", position: "bottom-36 left-16", size: "w-6 h-6", x: 70, y: -50, rotate: 120, duration: 1.0, delay: 0.3 }
+                { image: "//menu-card/imgs/choco.png", alt: "chocolate", position: "bottom-28 left-20", size: "w-8 h-8", x: 80, y: -35, rotate: 45, duration: 1.2, delay: 0.2 },
+                { image: "//menu-card/imgs/milk.png", alt: "milk", position: "bottom-32 left-24", size: "w-7 h-7", x: 90, y: -45, rotate: -30, duration: 1.4, delay: 0.4 },
+                { image: "//menu-card/imgs/choco.png", alt: "marshmallow", position: "bottom-24 left-28", size: "w-7 h-7", x: 100, y: -30, rotate: 90, duration: 1.6, delay: 0.6 },
+                { image: "//menu-card/imgs/milk.png", alt: "cinnamon", position: "bottom-36 left-16", size: "w-6 h-6", x: 70, y: -50, rotate: 120, duration: 1.0, delay: 0.3 }
             ]
         },
         {
@@ -162,12 +162,12 @@ function DrinksMenu() {
             textColor: "text-[#0277bd]",
             borderColor: "border-[#0277bd]",
             buttonColor: "bg-[#03a9f4]",
-            mainImage: "/assets/latte.png",
+            mainImage: "//menu-card/imgs/latte.png",
             ingredients: [
-                { image: "/assets/ice-cube.png", alt: "ice", position: "bottom-28 left-20", size: "w-8 h-8", x: 75, y: -40, rotate: 30, duration: 1.1, delay: 0.2 },
-                { image: "/assets/bean.png", alt: "coffee bean", position: "bottom-32 left-24", size: "w-7 h-7", x: 85, y: -50, rotate: -45, duration: 1.3, delay: 0.4 },
-                { image: "/assets/milk.png", alt: "milk", position: "bottom-24 left-28", size: "w-7 h-7", x: 95, y: -35, rotate: 60, duration: 1.5, delay: 0.6 },
-                { image: "/assets/ice-cube.png", alt: "ice", position: "bottom-36 left-16", size: "w-6 h-6", x: 70, y: -55, rotate: 120, duration: 1.0, delay: 0.3 }
+                { image: "//menu-card/imgs/ice-cube.png", alt: "ice", position: "bottom-28 left-20", size: "w-8 h-8", x: 75, y: -40, rotate: 30, duration: 1.1, delay: 0.2 },
+                { image: "//menu-card/imgs/bean.png", alt: "coffee bean", position: "bottom-32 left-24", size: "w-7 h-7", x: 85, y: -50, rotate: -45, duration: 1.3, delay: 0.4 },
+                { image: "//menu-card/imgs/milk.png", alt: "milk", position: "bottom-24 left-28", size: "w-7 h-7", x: 95, y: -35, rotate: 60, duration: 1.5, delay: 0.6 },
+                { image: "//menu-card/imgs/ice-cube.png", alt: "ice", position: "bottom-36 left-16", size: "w-6 h-6", x: 70, y: -55, rotate: 120, duration: 1.0, delay: 0.3 }
             ]
         }
     ];
